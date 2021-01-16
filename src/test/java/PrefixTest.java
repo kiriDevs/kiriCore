82779830d5afa1b8d9b6d927c66a506a6e6bc71b
kiriDevs <56218513+kiriDevs@users.noreply.github.com>
2021-01-17 00:59:55 +0100

    Added debug output to PrefixTest
    It now shows how many scenarios of what test file (at the moment only one exists) are already tested

--- src/test/java/PrefixTest.java
+++ src/test/java/PrefixTest.java
@@ -19,16 +19,19 @@ public class PrefixTest {
         playerExpectation = "§8[§r§l§2success§r§8]§r§a ";
         checkingPrefix = new Prefix("success", "2", "a");
         assert checkPrefix();
+        System.out.println("Successfully tested Prefix (1/3)!");
 
         consoleExpectation = "§8[§r§aerrorMaker§r§8]§r§4 ";
         playerExpectation = "§8[§r§l§aerrorMaker§r§8]§r§4 ";
         checkingPrefix = new Prefix("errorMaker", "a", "4");
         assert checkPrefix();
+        System.out.println("Successfully tested Prefix (2/3)!");
 
         consoleExpectation = "§8[§r§9secretMessageLol§r§8]§r§k ";
         playerExpectation = "§8[§r§l§9secretMessageLol§r§8]§r§k ";
         checkingPrefix = new Prefix("secretMessageLol", "9", "k");
         assert checkPrefix();
+        System.out.println("Successfully tested Prefix (3/3)!");
     }
 
 }
