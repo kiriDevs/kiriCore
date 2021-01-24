189c73b81e04733e55a41b8e867dc170cb9f4852
kiriDevs <56218513+kiriDevs@users.noreply.github.com>
2021-01-24 15:52:28 +0100

    style: Fixed typos and grammar in DummyInterface @ PublicAPI

--- src/main/java/de/kiridevs/kiricore/annotations/PublicAPI.java
+++ src/main/java/de/kiridevs/kiricore/annotations/PublicAPI.java
@@ -4,9 +4,9 @@ public @interface PublicAPI {
     /*
         This @interface is doing nothing (as you can see).
 
-        It can be used as an Entry Point for "unused"-inspection in IntelliJ
+        It can be used as an Entry Point for "unused"-inspection in IntelliJ.
         This prevents IntelliJ from showing "unused" warnings,
-          while not suppressing compiler warning.
+          while not suppressing compiler warnings.
         This behaviour makes sure VSCode doesn't throw warnings about
           a compiler warning being suppressed.
      */
