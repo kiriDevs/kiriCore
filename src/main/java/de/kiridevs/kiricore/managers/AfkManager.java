5640f4c943d0d5a1ed38afb34d2d835405712bef
kiriDevs <56218513+kiriDevs@users.noreply.github.com>
2021-01-24 15:37:24 +0100

    style: Enforcing 80col char-limit in AfkManager.java

--- src/main/java/de/kiridevs/kiricore/managers/AfkManager.java
+++ src/main/java/de/kiridevs/kiricore/managers/AfkManager.java
@@ -7,31 +7,45 @@ import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 
-/** A class with only static methods used to manage the AFK status of players using kiriCore's AFK system */
+/**
+ * A class with only static methods used to manage the AFK status of players
+ * using kiriCore's AFK system
+ */
+@SuppressWarnings("UnusedReturnValue")
 public class AfkManager {
+    @SuppressWarnings("FieldMayBeFinal")
     private static ArrayList<String> afkList = new ArrayList<>();
 
     // Methods for checking status
     /** Allows quick checking of the status of a player using the player's name
-     * @param playername The name (not display name) of the player to check the status of
+     * @param playername The name (not display name) of the player
+     *        to check the status of
      * @return true when the player IS marked as AFK right now
      *         false when the player is NOT marked as AFK right now
      */
-    @PublicAPI public static boolean isAfk(String playername) { return afkList.contains(playername); }
+    @PublicAPI public static boolean isAfk(String playername) {
+      return afkList.contains(playername);
+    }
 
-    /** Allows quick checking of the status of a player using the player's Player object
+    /**
+     * Allows quick checking of the status of a player using the player's
+     * Player object
+     *
      * @param player The Player element of the player to check the status of
      * @return true when the player IS marked as AFK right now
      *         false when the player is NOT marked as AFK right now
      */
-    @PublicAPI public static boolean isAfk(Player player) { return afkList.contains(player.getName()); }
+    @PublicAPI public static boolean isAfk(Player player) {
+      return afkList.contains(player.getName());
+    }
 
 
     // Methods for changing status
     public static boolean markAfk(Player player) {
         if (!isAfk(player)) {
             // Calling an event
-            PlayerChangeAfkStatusEvent event = new PlayerChangeAfkStatusEvent(player, true);
+            PlayerChangeAfkStatusEvent event = new PlayerChangeAfkStatusEvent
+                                               (player, true);
             Bukkit.getPluginManager().callEvent(event);
 
             afkList.add(player.getName()); // Adding Player to the AFKList
@@ -44,10 +58,11 @@ public class AfkManager {
     public static boolean markBack(Player player) {
         if (isAfk(player)) {
             // Calling an event
-            PlayerChangeAfkStatusEvent event = new PlayerChangeAfkStatusEvent(player, false);
+            PlayerChangeAfkStatusEvent event = new PlayerChangeAfkStatusEvent
+                                               (player, false);
             Bukkit.getPluginManager().callEvent(event);
 
-            afkList.remove(player.getName()); // Removing Player from the AFKList
+            afkList.remove(player.getName()); // Removing Player from AFKList
             return true; // Action completed
         } else {
             return false; // Nothing changed - Player wasn't AFK
@@ -64,8 +79,11 @@ public class AfkManager {
         }
     }
 
-    /** Getter for the full AFK list
-     * @return The current afkList including player names (not display names) of all players marked AFK at the moment
+    /**
+     * Getter for the full AFK list
+     *
+     * @return The current afkList including player names (not display names)
+     *         of all players marked AFK at the moment
      */
     public static ArrayList<String> getAfkList() { return afkList; }
 }
